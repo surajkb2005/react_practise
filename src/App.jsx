ea11fb49dd6beb3c17f5f0a39ea20425bb10ab7d
Suraj <skbanavalikar2105@gmail.com>
2026-03-04 21:54:50 +0530

    Added the cat image

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ function App() {
   return (
     <div>
       <h1>Hello World</h1>
+      <img src="https://www.alleycat.org/wp-content/uploads/2019/03/FELV-cat.jpg" alt="Cat" />
     </div>
   );
 }
