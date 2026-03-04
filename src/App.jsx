46f779e7f39e7831cef87c34f6a67221892a7981
Suraj <skbanavalikar2105@gmail.com>
2026-03-04 22:26:22 +0530

    detailed card

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,14 @@
 import React from 'react';
 import Card from './components/Card';
+import DetailedCard from './components/DetailsCard';
 
 function App() {
   return (
     <div>
       <h1>Hello World</h1>
-      <p>Cat 1</p>
       <Card />
-      <p>Cat 2</p>
-      <Card />
-      <p>Cat 3</p>
-      <Card />
-      <p>Cat 4</p>
-      <Card />
-      <p>Cat 5</p>
-      <Card />
+      <p>Cat</p>
+      <DetailedCard />
     </div>
   );
 }
---
+++ src/components/DetailsCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../index.css';
+
+export default function DetailsCard() {
+    return (
+        <div className="details_card">
+            <b>Cat Details</b>
+            <p>Name: Whiskers</p>
+            <p>Age: 3 years</p>
+            <p>Breed: Domestic Shorthair</p>
+            <p>Color: Tabby</p>
+        </div>
+    )
+}
