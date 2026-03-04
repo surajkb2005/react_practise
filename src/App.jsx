4112acf3a9e1cd6b263c7e72d5a9ec65536649e8
Suraj <skbanavalikar2105@gmail.com>
2026-03-04 22:05:01 +0530

    div and class added with some css to make it better

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,10 @@ function App() {
   return (
     <div>
       <h1>Hello World</h1>
-      <img src="https://www.alleycat.org/wp-content/uploads/2019/03/FELV-cat.jpg" alt="Cat" />
+      <div className="circle_card">
+        <img src="https://www.alleycat.org/wp-content/uploads/2019/03/FELV-cat.jpg" alt="Cat" ></img>
+      </div>
+
     </div>
   );
 }
