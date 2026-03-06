3fa5157a73e35d82f689d91319cd89ff4b76821a
Suraj <skbanavalikar2105@gmail.com>
2026-03-06 22:46:53 +0530

    made the inline functions

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,13 @@ import Card from './components/Card';
 import DetailedCard from './components/DetailsCard';
 import catsList from './data.js';
 
-function createCards(cats) {
-  return (
-    <Card
-      image={cats.image}
-      alt={cats.alt}
-    />
-  )
-}
-
 function App() {
   return (
     <div>
       <h1>Hello World</h1>
-      {catsList.map(createCards)}
+
+      {catsList.map((cats) => <Card image={cats.image} alt={cats.alt} />)}
+
       <p>Cat</p>
       <DetailedCard />
     </div>
